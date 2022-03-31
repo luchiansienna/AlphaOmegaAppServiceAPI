19083b6b29ad4d3ed8f0932d7addd4c8cf292062
luchiansienna@gmail.com <luchiansienna@gmail.com>
2022-03-31 16:55:03 +0300

    Cors allowed

--- Startup.cs
+++ Startup.cs
@@ -41,6 +41,7 @@ namespace AlphaOmegaAppServiceAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AlphaOmegaAppServiceAPI", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -78,13 +79,19 @@ namespace AlphaOmegaAppServiceAPI
                     }
                 });
             });
-            
+
             app.UseStatusCodePages();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
